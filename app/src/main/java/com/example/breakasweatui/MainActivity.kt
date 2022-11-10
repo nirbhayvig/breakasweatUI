b420feb0ee476150cf0e2124f4f03a1584d37b5f
Nirbhay <nirbhayvig43@gmail.com>
2022-11-10 16:50:34 -0600

    Basic page control set, nav TODO

--- app/src/main/java/com/example/breakasweatui/MainActivity.kt
+++ app/src/main/java/com/example/breakasweatui/MainActivity.kt
@@ -7,37 +7,84 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.breakasweatui.ui.theme.BreakaSweatUITheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BreakaSweatUITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                main(modifier = Modifier.fillMaxSize())
             }
+        }
+    }
+}
+@Composable
+fun main(modifier: Modifier = Modifier) {
+    
+    var shouldShowWelcome by remember { mutableStateOf(true) }
+    Surface(modifier) {
+        if (shouldShowWelcome) {
+            WelcomeScreen(onBeginClicked = {shouldShowWelcome = false})
+        } else {
+            BeginningWorkout()
+        }
+    }
+}
+
+
+@Composable
+fun WelcomeScreen(
+    onBeginClicked: () -> Unit,
+    modifier: Modifier = Modifier) {
+    // TODO: This state should be hoisted
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to Break a Sweat!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onBeginClicked
+        ) {
+            Text("Begin Workout")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BeginningWorkout(
+    modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Begin Workout:")
+        Button(
+            onClick = {}
+        ) {
+            Text("Start")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun DefaultPreview() {
+fun WelcomePreview() {
     BreakaSweatUITheme {
-        Greeting("Android")
+        WelcomeScreen(onBeginClicked = {})
     }
 }
